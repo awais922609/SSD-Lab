9e06cf0a9d2ca4e792a2768501ce0efb070bb706
Awais Sajid <69464258+awais922609@users.noreply.github.com>
2022-11-22 15:47:41 +0500

    Update PortScan.py

--- PortScan.py
+++ PortScan.py
@@ -3,7 +3,7 @@ from scapy.all import *
 ports = [25,80,53,443,445,8080,8443]
 
 def SynScan(host):
-    ans,unans = sr(IP(dst=host)/TCP(dport=ports,flags="S"),timeout=2,verbose=0)
+    ans = sr(IP(dst=host)/TCP(dport=ports,flags="S"),timeout=2,verbose=0) //unans
     print("Open ports at %s:" % host)
     for (s,r,) in ans:
         if s.haslayer(TCP) and r.haslayer(TCP):
@@ -14,8 +14,6 @@ def DNSScan(host):
     ans,unans = sr(IP(dst=host)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname="google.com")),timeout=2,verbose=0)
     if ans:
         print("DNS Server at %s"%host)
-    
 host = "8.8.8.8"
-
 SynScan(host)
 DNSScan(host)
